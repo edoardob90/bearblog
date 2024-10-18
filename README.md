a0fb0da75f90a7313d27cf1da7d3477d28d12b6f
Edoardo Baldi <edoardob90@gmail.com>
2024-10-18 23:12:43 +0200

    Init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Bearblog stuff
+
+This repo contains all the files related to my blog on [Bearblog.dev](https://notes.edoardob.im).
+
+- CSS files
+- JS snippets (usually to be wrapped in a `<script>` tag in the blog's footer)
+- Blog templates
---
+++ js/search.js
@@ -0,0 +1,26 @@
+// A simple search bar for the blog page
+// NOTE: search is limite to posts' titles
+
+if (
+  document.querySelector(".blog-posts") &&
+  document.body.classList.contains("blog")
+) {
+  document.querySelector("main").insertBefore(
+    Object.assign(document.createElement("input"), {
+      type: "text",
+      id: "searchInput",
+      placeholder: "Search...",
+      style: "display: block;",
+      oninput: (event) => {
+        document.querySelectorAll(".blog-posts li").forEach((post) => {
+          post.style.display = post.textContent
+            .toLowerCase()
+            .includes(event.target.value.toLowerCase())
+            ? ""
+            : "none";
+        });
+      },
+    }),
+    document.querySelector(".blog-posts"),
+  );
+}
